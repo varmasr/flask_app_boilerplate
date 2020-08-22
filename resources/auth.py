14940ca5d672cffc2576a4e30533aa6164892915
varmasr <srini.upwork@gmail.com>
2020-08-22 12:43:13 +0530

    exception handling

--- resources/auth.py
+++ resources/auth.py
@@ -2,28 +2,46 @@ from flask_restful import Resource
 from flask import request
 from flask_jwt_extended import create_access_token
 import datetime
+from mongoengine.errors import FieldDoesNotExist, NotUniqueError, DoesNotExist
 
+from .errors import SchemaValidationError, EmailAlreadyExistsError, InternalServerError,\
+                   UnauthorizedError
 from database.models import User
 
 class SignupApi(Resource):
     
     def post(self):
-        body = request.get_json()
-        user = User(**body)
-        user.hash_password()
-        user.save()
-        id = user.id
-        return {'id':str(id)}, 200
+        try:
+            body = request.get_json()
+            user = User(**body)
+            user.hash_password()
+            user.save()
+            id = user.id
+            return {'id':str(id)}, 200
+        except NotUniqueError:
+            raise EmailAlreadyExistsError
+
+        except FieldDoesNotExist:
+            raise SchemaValidationError
+        
+        except Exception as e:
+            raise InternalServerError
+
 
 class LoginApi(Resource):
 
     def post(self):
-        body = request.get_json()
-        user = User.objects.get(email=body.get('email'))
-        authorized = user.check_password(body.get('password'))
-        if not authorized:
-            return {'error': 'Email or password not valid'}, 401
-        
-        expires = datetime.timedelta(days=7)
-        access_token = create_access_token(identity=str(user.id), expires_delta=expires)
-        return {'access_token': access_token}, 200
+        try:
+            body = request.get_json()
+            user = User.objects.get(email=body.get('email'))
+            authorized = user.check_password(body.get('password'))
+            if not authorized:
+                raise UnauthorizedError
+            
+            expires = datetime.timedelta(days=7)
+            access_token = create_access_token(identity=str(user.id), expires_delta=expires)
+            return {'access_token': access_token}, 200
+        except (UnauthorizedError, DoesNotExist):
+            raise UnauthorizedError
+        except Exception as e:
+            raise InternalServerError
--- resources/movie_restful.py
+++ resources/movie_restful.py
@@ -1,8 +1,13 @@
 from flask import request, Response
 from flask_restful import Resource
 from flask_jwt_extended import jwt_required, get_jwt_identity
+from mongoengine.errors import FieldDoesNotExist, NotUniqueError, DoesNotExist,\
+                               ValidationError, InvalidQueryError
 
 from database.models import Movie,User
+from resources.errors import SchemaValidationError, MovieAlreadyExistsError, \
+                             InternalServerError, UpdatingMovieError, DeletingMovieError, \
+                             MovieNotExistsError
 
 
 
@@ -15,36 +20,59 @@ class MoviesApi(Resource):
 
     @jwt_required
     def post(self):
-        user_id = get_jwt_identity()
-        body = request.get_json()
-        user = User.objects.get(id=user_id)
-        movie = Movie(**body, added_by=user).save()
-        # push__movies ??
-        user.update(push__movies = movie)
-        user.save()
-        id = movie.id
-        return {'id': str(id)}, 200
+        try:
+            user_id = get_jwt_identity()
+            body = request.get_json()
+            user = User.objects.get(id=user_id)
+            movie = Movie(**body, added_by=user).save()
+            # push__movies ??
+            user.update(push__movies = movie)
+            user.save()
+            id = movie.id
+            return {'id': str(id)}, 200
+        except (FieldDoesNotExist, ValidationError):
+            raise SchemaValidationError
+        except NotUniqueError:
+            raise MovieAlreadyExistsError
+        except Exception as e:
+            raise InternalServerError
 
 class MovieApi(Resource):
     
     @jwt_required
     def put(self, id):
-        body = request.get_json()
-        user_id = get_jwt_identity()
-        movie = Movie.objects.get(id=id, added_by=user_id)
-        movie.update(**body)
-        return 'updated', 200
-    
+        try:
+            body = request.get_json()
+            user_id = get_jwt_identity()
+            movie = Movie.objects.get(id=id, added_by=user_id)
+            movie.update(**body)
+            return 'updated', 200
+        except InvalidQueryError:
+            raise SchemaValidationError
+        except DoesNotExist:
+            raise UpdatingMovieError
+        except Exception as e:
+            raise InternalServerError
+
     @jwt_required
     def delete(self, id):
-        user_id = get_jwt_identity()
-        movie = Movie.objects.get(id=id, added_by=user_id)
-        movie.delete()
-        return "Deleted", 200
+        try:
+            user_id = get_jwt_identity()
+            movie = Movie.objects.get(id=id, added_by=user_id)
+            movie.delete()
+            return "Deleted", 200
+        except DoesNotExist:
+            raise DeletingMovieError
+        except Exception as e:
+            raise InternalServerError
     
     def get(self, id):
-        movie = Movie.objects.get(id=id).to_json()
-        return Response(movie, mimetype="application/json", status=200)
-
+        try:
+            movie = Movie.objects.get(id=id).to_json()
+            return Response(movie, mimetype="application/json", status=200)
+        except DoesNotExist:
+            raise MovieNotExistsError
+        except Exception as e:
+            raise InternalServerError
 
     
