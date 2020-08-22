15c1df259958135fe0809943498ec55bafaa8768
varmasr <srini.upwork@gmail.com>
2020-08-22 11:58:59 +0530

    one-to-many relations between model objects

--- database/models.py
+++ database/models.py
@@ -1,18 +1,30 @@
 from .db import db
 from flask_bcrypt import generate_password_hash, check_password_hash
 
+
+# New Feature : User who created the movie only delete or edit the movie
+
 class Movie(db.Document):
     name = db.StringField(required=True, unique=True)
     casts = db.ListField(db.StringField(),required=True)
     genres = db.ListField(db.StringField(), required=True)
+    added_by = db.ReferenceField('User')
 
+   
 
 class User(db.Document):
     email = db.EmailField(required=True, unique=True)
     password = db.StringField(required=True, min_length=6)
+    #TODO : Need to understand in detail below code.
+    # If a movie is deleted then movie should be pulled from the user document
+    movies = db.ListField(db.ReferenceField('Movie', reverse_delete_rule=db.PULL))
 
     def hash_password(self):
         self.password = generate_password_hash(self.password).decode('utf-8')
 
     def check_password(self,password):
         return check_password_hash(self.password, password)
+
+
+# If a user is deleted then the movie created by the user is also deleted
+User.register_delete_rule(Movie, 'added_by', db.CASCADE)
--- resources/movie_restful.py
+++ resources/movie_restful.py
@@ -1,8 +1,8 @@
 from flask import request, Response
 from flask_restful import Resource
-from flask_jwt_extended import jwt_required
+from flask_jwt_extended import jwt_required, get_jwt_identity
 
-from database.models import Movie
+from database.models import Movie,User
 
 
 
@@ -15,8 +15,13 @@ class MoviesApi(Resource):
 
     @jwt_required
     def post(self):
+        user_id = get_jwt_identity()
         body = request.get_json()
-        movie = Movie(**body).save()
+        user = User.objects.get(id=user_id)
+        movie = Movie(**body, added_by=user).save()
+        # push__movies ??
+        user.update(push__movies = movie)
+        user.save()
         id = movie.id
         return {'id': str(id)}, 200
 
@@ -25,12 +30,16 @@ class MovieApi(Resource):
     @jwt_required
     def put(self, id):
         body = request.get_json()
-        Movie.objects.get(id=id).update(**body)
+        user_id = get_jwt_identity()
+        movie = Movie.objects.get(id=id, added_by=user_id)
+        movie.update(**body)
         return 'updated', 200
     
     @jwt_required
     def delete(self, id):
-        Movie.objects.get(id=id).delete()
+        user_id = get_jwt_identity()
+        movie = Movie.objects.get(id=id, added_by=user_id)
+        movie.delete()
         return "Deleted", 200
     
     def get(self, id):
